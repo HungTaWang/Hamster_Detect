04138277b5dbbc4c5701ebb65a33ea764751bfa9
HungTaWang <hungta.wang0118@gmail.com>
2026-05-24 12:23:38 +0800

    UPDATE

---
+++ vite.config.js
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  // 將 base 設定為你的 GitHub Repository 名稱
+  // 這樣 GitHub Pages 才能正確載入 JS, CSS 與圖片資源
+  base: '/Hamster_Detect/',
+});
